a3e064563bc71ca9afdecf17bbb2853693acae04
Kaushal Kundu <k.kaushalkundu@gmail.com>
2024-12-28 10:45:54 +0530

    navbar made responsive 'demo'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,25 @@
 import { Link } from "react-router-dom";
-
 import "../styles/Navbar.css";
+import { useState } from "react";
 
 function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <div id="navbar-container">
       <h1>Virtual Vogue</h1>
-      <nav id="navbar-links">
+      <button id="mobile-menu-toggle" onClick={toggleMobileMenu}>
+        {/* You can use an icon here (e.g., hamburger icon) */}
+        <span>&#9776;</span> {/* Hamburger icon */}
+      </button>
+      <nav
+        id="navbar-links"
+        className={isMobileMenuOpen ? "mobile-menu-open" : ""}
+      >
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -15,11 +28,12 @@ function Navbar() {
             <Link to="/shop">Shop</Link>
           </li>
           <li>
-            <Link to="/">Cart</Link>
+            <Link to="/cart">Cart</Link>
           </li>
         </ul>
       </nav>
     </div>
   );
 }
+
 export default Navbar;
